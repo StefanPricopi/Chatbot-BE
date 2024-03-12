cdcc69243b461c1677cb98a9376490ac37748f82
I511494 <m.huseynov@student.fontys.nl>
2024-03-12 11:32:14 +0100

    Configure CI/CD Pipeline

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+import nl.fontys.s3.Greeting;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Greeting greeting = new Greeting();
+        System.out.println(greeting.hello("Mahmud"));
+    }
+
+}
