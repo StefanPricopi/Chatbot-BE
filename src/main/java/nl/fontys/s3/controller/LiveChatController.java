06e981b352569624b81b91d4ad008b50f030ca87
Shels <524813@student.fontys.nl>
2024-05-05 18:25:52 +0100

    Not done yet!

--- src/main/java/nl/fontys/s3/controller/LiveChatController.java
+++ src/main/java/nl/fontys/s3/controller/LiveChatController.java
@@ -1,7 +1,10 @@
 package nl.fontys.s3.controller;
 
 import nl.fontys.s3.domain.ChatDomains.MessageRequest;
+import nl.fontys.s3.domain.LivechatDomains.ChatMessage;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
@@ -10,8 +13,17 @@ public class LiveChatController {
 
     @MessageMapping("/chat")
     @SendTo("/topic/messages")
-    public MessageRequest send(MessageRequest messageRequest)
+    public MessageRequest send(ChatMessage chatMessage) //MessageRequest messageRequest
     {
-        return messageRequest;
+        System.out.println(chatMessage);
+        return null;
     }
+
+    @MessageMapping("/chats/{chatid}/queue/inboxmessages")
+    @SendTo("/topic/messages")
+    public void chat(@DestinationVariable("chatid") String id)
+    {
+        System.out.print(id);
+    }
+
 }
