b04bf8e1d8c236137ca87d814c974d0282b74b5e
Shels <i524813@student.fontys.nl>
2024-05-05 21:16:58 +0200

    Made some minor changes to the application. Chatbot works now on FE & BE

--- src/main/java/nl/fontys/s3/controller/LiveChatController.java
+++ src/main/java/nl/fontys/s3/controller/LiveChatController.java
@@ -1,15 +1,20 @@
 package nl.fontys.s3.controller;
 
+import lombok.AllArgsConstructor;
 import nl.fontys.s3.domain.ChatDomains.MessageRequest;
 import nl.fontys.s3.domain.LivechatDomains.ChatMessage;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
+@AllArgsConstructor
 @Controller
 public class LiveChatController {
+
+    private final SimpMessagingTemplate messagingTemplate;
 
     @MessageMapping("/chat")
     @SendTo("/topic/messages")
@@ -19,11 +24,14 @@ public class LiveChatController {
         return null;
     }
 
-    @MessageMapping("/chats/{chatid}/queue/inboxmessages")
+    @MessageMapping("/chats/{chatid}")
     @SendTo("/topic/messages")
-    public void chat(@DestinationVariable("chatid") String id)
+    public void chat(@DestinationVariable("chatid") String id, @Payload String msg)
     {
         System.out.print(id);
+        System.out.println(msg);
+
+        messagingTemplate.convertAndSend(1);
     }
 
 }
