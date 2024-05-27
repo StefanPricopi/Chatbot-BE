bcb7423295637048cc55c180507ccc8d0731bfea
Alex Hall <519651@student.fontys.nl>
2024-05-27 22:29:42 +0200

    changed userid

--- src/main/java/nl/fontys/s3/domain/users/UpdateUserRequest.java
+++ src/main/java/nl/fontys/s3/domain/users/UpdateUserRequest.java
@@ -11,7 +11,7 @@ import java.util.Set;
 @Getter
 @Setter
 public class UpdateUserRequest {
-    private Long userId;
+    private Long userid;
     @NotBlank
     private String username;
     @NotBlank
